058877a34d4f58bacc83c24e9c96cfcff6ab7c65
valet noir <valetnoir.gaming@gmail.com>
2025-10-10 11:01:11 +0200

    added main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace USRS45_Combat
+{
+    class USRS45_Combat
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
