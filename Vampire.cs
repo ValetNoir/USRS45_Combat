ae5fdf481bf07e38cdd075983c7751c6e374d3b3
ElliotN <nedelliot84@gmail.com>
2025-10-13 16:10:16 +0200

    +Vampire Class

---
+++ Vampire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace USRS45_Combat
+{
+    public class Vampire : Character
+    {
+        public Vampire()
+        {
+            Health = 2;
+            Damage = 1;
+            Nom = "skibidiVampire";
+        }
+
+        public override void Special(Character target)
+        {
+            base.Special(target);
+            Health++;
+            target.TakeDamage(1);
+            Console.WriteLine($"{this.Nom} a volé une vie de {target.Nom}.");
+        }
+    }
+}
